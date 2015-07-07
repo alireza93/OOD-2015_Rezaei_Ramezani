8c0ef2b27bcdb5967f60d85b356b1d05e7463a7b
hamid <mrramazani@gmail.com>
2015-07-08 02:33:19 +0430

    Few bugs removed.

--- code/src/Main.java
+++ code/src/Main.java
@@ -8,7 +8,6 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         EventQueue.invokeLater(new Runnable() {
 
             @Override
