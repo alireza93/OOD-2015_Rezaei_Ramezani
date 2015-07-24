a0c8535744345eaa2e5038b5df0e83cbc8bb5359
hamid <mrramazani@yahoo.com>
2015-07-24 16:52:10 -0700

    افزودن maven و وابستگی به mongodb java driver

---
+++ code/src/main/java/content/CommentRepository.java
@@ -0,0 +1,11 @@
+package content;
+
+import com.mongodb.DBCollection;
+
+/**
+ * @author Hamid Ramazani (mrramazani@gmail.com)
+ *         Created on 7/23/2015
+ */
+public class CommentRepository {
+//    DBCollection commentCollection =
+}
