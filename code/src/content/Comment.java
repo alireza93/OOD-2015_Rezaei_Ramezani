f902d67250dc7669a236503a3acf9a69a0ddb46a
hamid <mrramazani@yahoo.com>
2015-07-24 17:24:47 -0700

    افزودن maven و وابستگی به mongodb java driver

---
+++ code/src/content/Comment.java
@@ -0,0 +1,8 @@
+package content;
+
+/**
+ * @author Hamid Ramazani (mrramazani@gmail.com)
+ *         Created on 7/23/2015
+ */
+public class Comment {
+}
---
+++ code/src/user/User.java
@@ -0,0 +1,8 @@
+package user;
+
+/**
+ * @author Hamid Ramazani (mrramazani@gmail.com)
+ *         Created on 7/21/2015
+ */
+public class User {
+}
